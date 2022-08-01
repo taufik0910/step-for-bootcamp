02e3d2cdc8ef7d0e5abc53008198b033ec02fb73
taufik0910 <taufik.cradle@gmail.com>
2022-08-01 14:06:16 +0700

    konsep belajar tentang constructor(primary constructor) dan blok penginisiasi(init) pada kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,9 @@
 fun main(args: Array<String>) {
-//    println("Hello World!")
-    val murid = Murid("abika", 23)
-    println("nama murid nya: ${murid.nama}")
-    println("umur murid nya : ${murid.umur}")
+//    sintax dibawah merupakan pemanggilan object pada Class murid
+    val murid1 = Murid("isna", 23)
+    val murid2 = Murid("supri")
+    val murid3 = Murid()
 
 
 
-    /*berikut merupakan contoh penerapan konsep sederhana dalam kotlin, hal ini
-    * dibuat untuk latihan dalam step awal dalam megikuti kelas bootcamp kelas andriod enginner
-    * di binar akademy
-    *
-    * step ini merupakan cara untuk mempelajari konsep constructor primary
-    * */
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//    println("Program arguments: ${args.joinToString()}")
 }
--- src/main/kotlin/Murid.kt
+++ src/main/kotlin/Murid.kt
@@ -1,5 +1,24 @@
-/*class terlampir merupakan contoh belajar constructor primary
-* dengan menambahkan variable dalam nama class*/
-class Murid( val nama : String, var umur : Int) {
+class Murid(val nama: String ="Murid", var umur : Int= 23)
+//sintax di atas merupakan penerapan konsep primary constructor
+{
+    val murNama : String
+    var murUmur : Int
 
+    init {
+//        sintax ini merupakan penerapan method init (standard library)
+        if (nama == "Murid"){
+            murNama = "Farid".toUpperCase()
+            murUmur = 23
+        }else{
+            murNama =nama.toUpperCase()
+            murUmur = umur
+        }
+        println("Nama Murid : $murNama")
+        println("Umur Murid : $murUmur")
+    }
+
+    /*sintak ini merupakan konsep penerapan konsep init dan primary constructor
+    * dalam contoh berikut, kita memiliki blok penginisiasi yang telah kita nyatakan dalam constructor
+    * menggunakan INIT. dalam blok ini,
+    * kita dapat memiliki logika inisialisasi tambahan sesuai dengan kita kehendaki */
 }
